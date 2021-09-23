9888d945307a0d826f4a53c1f5f4ea3ec8a70063
Göksel Kınalı <carryvayne12211@gmail.com>
2021-09-23 14:19:33 +0300

    Arranged multiple circles.

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -12,19 +12,40 @@ background(0);
       Third circle:4 (sin * 5x ) / 5*PI
   */
 
-function setupHelper(){
-  background(255 ,255,255);
-  translate(200,200);
-  stroke(255);
-  fill(0);
+function FourierSolves(){
+  n=1;
+  x=0;
+  y=0;
+  for(let i=0;i<23;i++){
+  radius=(4/(Math.PI*n)) * 50;
+  previousx=x;
+  previousy=y;
+  ellipse(x,y,2*radius);
+    
+  noFill();
+
+  x= radius*cos(angle*n)+x;
+  y=radius*sin(angle*n)+y;
+
+
+n=n+2;
+
+
+line(x,y,previousx,previousy);
+stroke(255,0,0);
+line(0,0,x,y);
+
+}
+wave.unshift(y);
 
 }
 function draw() {
   
   setupHelper();
-  firstEllipse();
+  FourierSolves();
   waveDrawer();
-  angle=angle+0.1;
+  waveCleaner();
+  angle=angle+0.04;
 
 }
 
@@ -36,32 +57,25 @@ function waveDrawer()
     noFill();
     for( let i=0;i<wave.length;i++){
     vertex(i,wave[i]);
-  stroke(0);
+  
       line(x-200,y,0,wave[0]);
 
     }
 
   endShape();
 
-
-
-
 }
 
+function waveCleaner(){
+  if(wave.length>300){
+    wave.pop();
+  }
+}
 
-
-function firstEllipse(){
-   ellipse(0,0,radius*2);
-   x=radius*cos(angle);
-   y=radius*sin(angle);
-  
-
-   
-  wave.unshift(y);
-  
-  ellipse(x,y,5);
-  stroke(255,0,0);
-  line(0,0,x,y);
-
+function setupHelper(){
+  background(255 ,255,255);
+  translate(200,200);
+  stroke(255);
+  fill(0);
 
 }
