06d15e69afe1598152e71a36527374b4d2f5b60f
Göksel Kınalı <carryvayne12211@gmail.com>
2021-09-23 14:36:16 +0300

    Added slider for amount circle.

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,44 +1,24 @@
+
+ let sliderForAmountCircle=5;
+ let radius=50;
+ let angle=0;
+ let wave=[];
+ let x,y;
+ let boundry=5;
+
 function setup() {
 background(0);
    createCanvas(1000, 400);
+  
+   sliderForAmountCircle = createSlider(0, 255, 100);
+   sliderForAmountCircle.position(130, 300);
   }
-
-  let radius=50;
-  let angle=0;
-  let wave=[];
-  let x,y;
+ 
   /*  First circle:4 (sin * x ) / PI 
       Second circle:4 (sin * 3x ) / 3*PI
       Third circle:4 (sin * 5x ) / 5*PI
   */
-
-function FourierSolves(){
-  n=1;
-  x=0;
-  y=0;
-  for(let i=0;i<23;i++){
-  radius=(4/(Math.PI*n)) * 50;
-  previousx=x;
-  previousy=y;
-  ellipse(x,y,2*radius);
-    
-  noFill();
-
-  x= radius*cos(angle*n)+x;
-  y=radius*sin(angle*n)+y;
-
-
-n=n+2;
-
-
-line(x,y,previousx,previousy);
-stroke(255,0,0);
-line(0,0,x,y);
-
-}
-wave.unshift(y);
-
-}
+ 
 function draw() {
   
   setupHelper();
@@ -49,6 +29,53 @@ function draw() {
 
 }
 
+function addSliderForAmountCircle(){
+  boundry=sliderForAmountCircle.value();
+  if(boundry==0)
+  boundry=1;
+}
+
+
+function setupForFourierSolves(){
+  n=1;
+  x=0;
+  y=0;
+
+}
+function RegisterCurrentXandYtoPrevious(){
+  previousx=x;
+  previousy=y;
+  
+}
+function FourierSolves(){
+  setupForFourierSolves();
+  addSliderForAmountCircle();
+  
+  for(let i=0;i<boundry;i++){
+  radius=(4/(Math.PI*n)) * 50;
+  RegisterCurrentXandYtoPrevious();
+  ellipse(x,y,2*radius);
+    
+  noFill();
+
+  x= radius*cos(angle*n)+x;
+  y=radius*sin(angle*n)+y;
+
+  drawLineCircleCenterToCircleCenter();
+  n=n+2;
+
+}
+wave.unshift(y);
+
+}
+
+function drawLineCircleCenterToCircleCenter(){
+
+  line(x,y,previousx,previousy);
+  stroke(255,0,0);
+  line(0,0,x,y);
+  
+}
 function waveDrawer()
 {
 
